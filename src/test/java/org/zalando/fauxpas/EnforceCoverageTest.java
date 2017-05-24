762572037e97c53c55af44b7c14afe1533d1f31f
Willi Schönborn <willi.schoenborn@zalando.de>
2017-05-24 09:19:16 +0200

    Revert "Removed com.google.gag dependency"
    This reverts commit a2e5a4aa208c7ffd226883af70aa04e855b1583a.

--- src/test/java/org/zalando/fauxpas/EnforceCoverageTest.java
+++ src/test/java/org/zalando/fauxpas/EnforceCoverageTest.java
@@ -1,9 +1,13 @@
 package org.zalando.fauxpas;
 
+import com.google.gag.annotation.remark.Hack;
+import com.google.gag.annotation.remark.OhNoYouDidnt;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
+@Hack
+@OhNoYouDidnt
 @RunWith(JUnitPlatform.class)
 public final class EnforceCoverageTest {
 
