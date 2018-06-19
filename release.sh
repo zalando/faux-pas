a3f5b0b2bc2125feff093d0df1d289a63175d8ac
Willi Schönborn <willi.schoenborn@zalando.de>
2018-06-19 11:21:45 +0200

    Simplified release script

--- release.sh
+++ release.sh
@@ -2,23 +2,7 @@
 
 : ${1?"Usage: $0 <major|minor|patch>"}
 
-./mvnw scm:check-local-modification
+latest=$(git describe --abbrev=0 || echo 0.0.0)
+release=$(semver ${latest} -i $1 --preid RC)
 
-current=$(git describe --abbrev=0 || echo 0.0.0)
-release=$(semver ${current} -i $1 --preid RC)
-next=$(semver ${release} -i minor)
-
-# release
-./mvnw versions:set -D newVersion=${release}
-git add $(find . -name pom.xml)
-git commit -m "Release ${release}"
-./mvnw clean deploy -P release
-./mvnw scm:tag
-
-# next development version
-./mvnw versions:set -D newVersion=${next}-SNAPSHOT
-git add $(find . -name pom.xml)
-git commit -m "Development ${next}-SNAPSHOT"
-
-git push
-git push --tags
+./mvnw scm:check-local-modification clean deploy scm:tag -P release -D revision=${release} -D tag=${release}
